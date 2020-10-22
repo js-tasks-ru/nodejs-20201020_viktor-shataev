6779ecb67e7c4dc4513f329ab1c843540c19a829
shataev <vityashataev@gmail.com>
2020-10-22 22:58:50 +0300

    add solution for 01-intro 01-sum

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,13 @@
-function sum(a, b) {
-  /* ваш код */
+function sum ( a, b ) {
+	if ( !isNumber( a ) || !isNumber( b ) ) {
+		throw new TypeError;
+	}
+
+	return a + b;
+}
+
+function isNumber ( val ) {
+	return ( typeof val ) === 'number' && !Number.isNaN( val ) && isFinite( val );
 }
 
 module.exports = sum;
