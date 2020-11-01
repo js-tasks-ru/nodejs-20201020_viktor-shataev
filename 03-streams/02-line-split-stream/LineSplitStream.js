0177f4e6883786118b28b736721257aec6cde50d
shataev <vityashataev@gmail.com>
2020-11-01 18:17:58 +0300

    fix solution for 03-streams 02-line-split-stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -6,21 +6,34 @@ class LineSplitStream extends stream.Transform {
 		super( options );
 
 		this.options = options;
-		this.lineString = '';
+		this.restString = '';
 	}
 
 	_transform ( chunk, encoding, callback ) {
-		this.lineString += chunk.toString( this.options.encoding );
+		const lineSeparator = os.EOL;
+		const lineString = this.restString + chunk.toString( this.options.encoding );
+		let lineArr = lineString.split( lineSeparator );
+
+		/*
+		Если чанк заканчивается не переносом строки, а строкой
+		значит окончание/продолжение этой строки будет передаваться в следующем чанке.
+		Поэтому сохраняем эту строку в this.restString, чтобы склеить ее с окончанием/продолжением
+		при следующем вызове метода _transform
+		*/
+		if (lineArr[lineArr.length-1] !== ''){
+			this.restString = lineArr.pop();
+		}
+
+		lineArr.map(lineStr => {
+			this.push( lineStr );
+		} );
 
 		callback();
 	}
 
 	_flush ( callback ) {
-		const lineSeparator = os.EOL;
-
-		this.lineString.split( lineSeparator ).map( lineStr => {
-			this.push( lineStr );
-		} );
+		this.push( this.restString );
+		this.restString = '';
 
 		callback();
 	}
